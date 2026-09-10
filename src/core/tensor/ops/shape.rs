c686df78b71d741e304cb48f8e6c855e1df7e9a3
Luca Olivieri <luca.olivieri@mail.polimi.it>
2026-09-10 22:30:58 +0200

    made some improvements to broadcast()

--- src/core/tensor/ops/shape.rs
+++ src/core/tensor/ops/shape.rs
@@ -6,8 +6,8 @@ use crate::core::tensor::AbstractTensor;
 
 use crate::core::autograd::grad_fn::GradFnTrait;
 use crate::core::autograd::ops::shape::{
-    BackwardCopyD, BackwardExpand, BackwardSqueeze, BackwardTranspose, BackwardUnsqueeze, CopyDOp,
-    ExpandOp, SqueezeOp, TransposeOp, UnsqueezeOp,
+    BackwardBroadcast, BackwardCopyD, BackwardExpand, BackwardSqueeze, BackwardTranspose,
+    BackwardUnsqueeze, BroadcastOp, CopyDOp, ExpandOp, SqueezeOp, TransposeOp, UnsqueezeOp,
 };
 use crate::core::storage::TensorStorage;
 use crate::core::tensor::ops::math::apply_tensor_op;
@@ -85,6 +85,24 @@ impl GraphTensor {
         )
     }
 
+    pub fn broadcast(&self, b: &GraphTensor) -> GraphTensor {
+        Self::broadcast_to_shape(&self, &b.shape())
+    }
+
+    pub fn broadcast_to_shape(&self, target_shape: &Vec<usize>) -> GraphTensor {
+        apply_tensor_op(
+            |ops: &[&TensorStorage; 1]| TensorStorage::broadcast_to_shape(ops[0], target_shape),
+            Some(|operands: [GraphTensor; 1]| {
+                let old_shape = operands[0].shape().clone();
+                Box::new(BackwardBroadcast {
+                    operands,
+                    op: BroadcastOp { old_shape },
+                }) as Box<dyn GradFnTrait>
+            }),
+            &[self],
+        )
+    }
+
     /// Stack a list of tensors along a new dimension 0.
     ///
     /// All tensors must have the same shape. The output has shape
@@ -108,5 +126,42 @@ impl GraphTensor {
         GraphTensor {
             node: Rc::new(out_node),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn broadcast_forward() {
+        let a = GraphTensor::from_vec(vec![1.0, 2.0, 3.0], false);
+        let b = a.broadcast_to_shape(&vec![2, 3]);
+
+        assert_eq!(b.shape(), &vec![2, 3]);
+        assert_eq!(*b.at(&vec![0, 0]), 1.0);
+        assert_eq!(*b.at(&vec![1, 1]), 2.0);
+        assert_eq!(*b.at(&vec![1, 2]), 3.0);
+    }
+
+    #[test]
+    fn broadcast_sums_grad_over_expanded_axes() {
+        let a = GraphTensor::from_vec(vec![1.0, 2.0, 3.0], true);
+        let b = a.broadcast_to_shape(&vec![2, 3]);
+
+        let grads = b.backward(true);
+        let da = grads.get(&a.to_key()).unwrap();
+
+        assert_eq!(da.shape(), &vec![3]);
+        assert_eq!(*da.at(&vec![0]), 2.0);
+        assert_eq!(*da.at(&vec![1]), 2.0);
+        assert_eq!(*da.at(&vec![2]), 2.0);
+    }
+
+    #[test]
+    #[should_panic]
+    fn broadcast_incompatible_shape_panics() {
+        let a = GraphTensor::from_vec(vec![1.0, 2.0, 3.0], false);
+        a.broadcast_to_shape(&vec![2, 2]);
     }
 }
